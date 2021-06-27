7e2482318dbc8dc9e0e00a40a3164626a08426ae
Marta Skibicka <marta.skibicka.contact@gmail.com>
2021-06-27 10:03:04 +0200

    Odczytywanie parametrów do zmiany z terminala

--- reader.py
+++ reader.py
@@ -2,17 +2,17 @@ import csv, pickle, json, sys
 
 file_content = []
 
-with open("ford_escort.csv") as file:
+with open(sys.argv[1], "r") as file:
     reader = csv.reader(file)
     for line in reader:
         file_content.append(line)
 
-file_content[2][1] = 27
-print(file_content[2][1])
+for param in sys.argv[3:]:
+    print(param.split(","))
+    param_list = param.split(",")
+    file_content[int(param_list[0])-1][int(param_list[1])-1] = param_list[2]
 
-with open("ford_escort_2.csv", "w", newline="") as file:
+with open(sys.argv[2], "w", newline="") as file:
     writer = csv.writer(file)
     for line in file_content:
         writer.writerow(line)
-
-line_count = 0
